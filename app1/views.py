77c12df44c5f7cb8d116b003bb907aa11b381c9b
mitesh kumar <miteshsutha7297@gmail.com>
2022-03-25 16:13:14 +0530

    db connection and operation

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.db import connection
 
 
 # Create your views here.
@@ -10,5 +11,17 @@ def xyz(request):
 
 
 def signUp(request):
-    print("signUp method is working");
-    return render(request, "index.html")
+    email = request.GET['email']
+    psw = request.GET['psw']
+    cursor = connection.cursor()
+    query = "insert into users (email,psw) values(%s,%s)"
+    value = (email, psw)
+    cursor.execute(query, value)
+    print(cursor.rowcount)
+    data = {"email": email, "psw": psw}
+    return render(request, "first.html", data)
+    # query = '''Select * from city where id=11'''
+    # row = cursor.fetchone()
+    # print(row)
+    # query = "Select * from city where name ='"+email+"'"
+
